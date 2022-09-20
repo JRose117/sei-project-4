3e8bc3c871a209b3beb70aa3fdd8381c606bed9a
JRose117 <joerose123@hotmail.co.uk>
2022-09-20 15:10:10 +0100

    ready to deploy

--- project/settings.py
+++ project/settings.py
@@ -141,7 +141,7 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.BrowsableAPIRenderer',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'jwt_auth.authentication.JWTAuthentication'
     ]
 }
 
